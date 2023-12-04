eadc69391a4042cf0c204c36a6bec0f791ef175c
Javi <angeljavier.jimenez.torres@alumnos.upm.es>
2023-12-05 00:06:07 +0100

    twelve commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -33,21 +33,20 @@ const Home = ({ name, title }) => {
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <a href="#nextSection">
+          <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        </a>
       </div>
     </section>
   );
 };
 
-Home.defaultProps = {
-  name: "",
-  title: "",
-};
 
 Home.defaultProps = {
   name: "",
   title: "",
 };
+
 
 Home.propTypes = {
   name: PropTypes.string.isRequired,
