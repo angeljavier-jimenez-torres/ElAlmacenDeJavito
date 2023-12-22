8029895541db5010582b5f315c8aff2cd24f5219
Javier <angeljavier.jimenez.torres@alumnos.upm.es>
2023-12-22 17:35:56 +0100

    Comm

--- src/index.js
+++ src/index.js
@@ -9,19 +9,3 @@ import App from "./App";
 
 render(<App></App>, document.getElementById("app"));
 
-// Obtén el elemento body
-var body = document.body;
-
-// Obtén el botón de cambio de modo
-var button = document.getElementById('dark-mode-button');
-
-// Agrega un evento de clic al botón
-button.addEventListener('click', function() {
-    // Si el body tiene la clase 'dark-mode', remuévela; de lo contrario, agrégala
-    if (body.classList.contains('dark-mode')) {
-        body.classList.remove('dark-mode');
-    } else {
-        body.classList.add('dark-mode');
-    }
-});
-
